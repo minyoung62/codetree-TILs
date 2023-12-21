036fbf0559c3b35660dcb715965cf6fbd9934051
minyoung <61530368+minyoung62@users.noreply.github.com>
2023-12-21 19:37:44 +0900

    Title: 문자에 따른 명령 2, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231221/문자에 따른 명령 2/text-based-commands2.py
+++ 231221/문자에 따른 명령 2/text-based-commands2.py
@@ -3,14 +3,13 @@ command = input()
 n = len(command)
 
 dxs = [1, 0, -1, 0]
-dys = [0, -1, 0, 1]
+dys = [0, 1, 0, -1]
 x, y = 0, 0
 curr_dir = 3
 for c in command:
     if c == 'L':
         curr_dir = (curr_dir + 3) % 4
     elif c == 'R':
-        curr_dir = (curr_dir + 1) % 4
         curr_dir = (curr_dir + 1) % 4
     else:
         x += dxs[curr_dir]
