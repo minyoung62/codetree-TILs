d2bea13273fac8ddcfabd838f1ffcd382f7acb02
minyoung <61530368+minyoung62@users.noreply.github.com>
2023-12-22 19:35:49 +0900

    Title: 최고의 33위치, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231222/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+a = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+ans = 0
+
+def getNum(i, j):
+    cnt = 0
+    for x in range(i + 3):
+        for y in range(i + 3):
+            if a[x][y] == 1:
+                cnt += 1
+    return cnt
+
+
+for i in range(n-2):
+    for j in range(n-2):
+        cnt = getNum(i, j)
+        ans = max(ans, cnt)
+print(ans)
