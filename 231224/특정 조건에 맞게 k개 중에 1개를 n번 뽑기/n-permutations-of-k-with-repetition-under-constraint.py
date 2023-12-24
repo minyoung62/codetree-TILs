836fbb215808b6db0307844f57e8b52b41c22840
minyoung <61530368+minyoung62@users.noreply.github.com>
2023-12-24 19:20:44 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231224/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 231224/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -8,9 +8,13 @@ def choose(cnt):
         return
     else:
         for i in range(1, k + 1):
-            if cnt == 0 or cnt == 1 or (selected[cnt-2] != i and selected[cnt-1] != i):
-                selected[cnt] = i
-                choose(cnt + 1)
+            if cnt >= 2 and (selected[cnt-2] != i and selected[cnt-1] != i):
+                continue
+            selected[cnt] = i
+            choose(cnt + 1)
+            # if cnt == 0 or cnt == 1 or (selected[cnt-2] != i and selected[cnt-1] != i):
+            #     selected[cnt] = i
+            #     choose(cnt + 1)
             
 
 choose(0)
